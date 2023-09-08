cbaaa95ede51ff4bc18db363aa53b2ce69d0e855
Jazaltron <jazaltron10@gmail.com>
2023-09-08 16:29:09 +0100

    Ex_1

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import {useState, useEffect} from 'react'
+// import {BrowserRouter as Router, Route} from 'react-router-dom'
+// import Header from "./components/Header"
+// import Footer from "./components/Footer"
+// import Tasks from "./components/Tasks"
+// import AddTask from "./components/AddTask"
+// import About from "./components/About"
+
+const App = () =>{
+  return (
+    "Hello World"
+  );
+}
+
+export default App;
+
+
+
+
+
